a7f28be1520401d0657efd776f7d6d67cd31e90b
vinside2000 <928835512@qq.com>
2022-03-12 15:17:10 +0800

    api修改

--- usx-oj-system/vue.config.js
+++ usx-oj-system/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
         // port: 9876,
         proxy: {                 //设置代理，必须填
             '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
+                // target: 'http://114.55.38.79:9090',     //代理的目标地址
                 target: 'http://localhost:9090',     //代理的目标地址
                 changeOrigin: true,              //是否设置同源，输入是的
                 pathRewrite: {                   //路径重写
